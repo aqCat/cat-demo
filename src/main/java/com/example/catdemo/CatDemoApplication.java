58435f51cae1894ee480a996b8d5ae8ea149abf3
caijianhua <cjh13720@cnki.net>
2020-09-30 16:09:53 +0800

    修改

--- src/main/java/com/example/catdemo/CatDemoApplication.java
+++ src/main/java/com/example/catdemo/CatDemoApplication.java
@@ -9,6 +9,7 @@ public class CatDemoApplication {
     public static void main(String[] args) {
         System.out.println("小李同学的修改1");
         System.out.println("小李同学的修改ssss");
+        System.out.println("cccc");
         SpringApplication.run(CatDemoApplication.class, args);
     }
 
